306e8b428c24bdba8f781ec8f7335e4e19125028
Johan F. <johan.artmat@gmail.com>
2024-07-10 23:40:08 -0500

    [FEAT] Student Page
    Creation of the student page with projects preview.

--- src/components/ResearchProjectCard.tsx
+++ src/components/ResearchProjectCard.tsx
@@ -1,10 +1,80 @@
-function ResearchProjectCard() {
+import CircularProgress from "@mui/material/CircularProgress";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+interface ResearchProjectCardProps {
+  name: string;
+  description: string;
+  rating: number;
+}
+
+function ResearchProjectCard({
+  name,
+  description,
+  rating,
+}: ResearchProjectCardProps) {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-tertiary-dark">
-        ResearchProjectCard
-      </h1>
-    </>
+    <Button
+      color="inherit"
+      sx={{
+        backgroundColor: "#C80036",
+        borderRadius: "24px",
+        marginBottom: "8px",
+      }}
+      className="text-quaternary-dark grid grid-cols-5"
+    >
+      <span className="col-span-1 flex justify-between items-center pl-8 w-[70%]">
+        <span>
+          <Box
+            sx={{
+              position: "relative",
+              display: "inline-flex",
+              scale: "2",
+              color: "#FF6969",
+            }}
+          >
+            <CircularProgress
+              variant="determinate"
+              value={rating}
+              color="inherit"
+            />
+            <Box
+              sx={{
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                position: "absolute",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="caption" component="div">{`${Math.round(
+                rating
+              )}`}</Typography>
+            </Box>
+          </Box>
+        </span>
+        <p className="flex justify-center w-[80%]">
+          <Typography
+            sx={{
+              color: "#FFF5E1",
+              flexGrow: 1,
+              fontWeight: 700,
+              letterSpacing: ".1rem",
+              textDecoration: "none",
+            }}
+          >
+            {name}
+          </Typography>
+        </p>
+      </span>
+      <span className="col-span-4 p-8 text-quaternary-dark text-xs">
+        {description}
+      </span>
+    </Button>
   );
 }
 
--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -1,11 +1,88 @@
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+
 import UserLayout from "../layouts/UserLayout";
+import ResearchProjectCard from "../components/ResearchProjectCard";
+
+const projects = [
+  {
+    name: "Renewable Energy Integration",
+    description:
+      "Analyzes the challenges and solutions for integrating renewable energy sources into the national grid.",
+    rating: 92,
+  },
+  {
+    name: "AI in Healthcare",
+    description:
+      "Explores the use of artificial intelligence to improve diagnostic accuracy and patient outcomes.",
+    rating: 87,
+  },
+  {
+    name: "Urban Wildlife Conservation",
+    description:
+      "Investigates the impact of urbanization on local wildlife and proposes conservation strategies.",
+    rating: 78,
+  },
+];
 
 function Student() {
   return (
     <UserLayout>
-      <h1 className="text-3xl font-bold underline text-tertiary-dark">
-        Student
-      </h1>
+      <div className="w-full h-full p-[4%]">
+        <section className="flex justify-between items-center">
+          <Typography
+            className="gradient-text"
+            sx={{
+              fontWeight: 900,
+              letterSpacing: ".1rem",
+              fontSize: { xs: "2rem", md: "3rem" },
+            }}
+          >
+            Projects
+          </Typography>
+
+          <Paper
+            component="form"
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              width: 400,
+              height: "40px",
+              backgroundColor: "#FF6969",
+              borderBottom: "2px solid #FFF5E1",
+            }}
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1, color: "#FFF5E1" }}
+              placeholder="Search some project..."
+              inputProps={{ "aria-label": "search some project" }}
+            />
+            <IconButton
+              type="button"
+              sx={{ p: "10px", color: "#FFF5E1" }}
+              aria-label="search"
+            >
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </section>
+        <section className="w-full h-[90%] p-[4%]">
+          {projects.map((project, index) => {
+            return (
+              <ResearchProjectCard
+                key={`project-${index}`}
+                name={`${project.name}`}
+                description={`${project.description}`}
+                rating={project.rating}
+              />
+            );
+          })}
+        </section>
+      </div>
     </UserLayout>
   );
 }
